aaaae334da43d32ba8efafc032d7686085298a60
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2025-05-14 17:15:01 +0300

    .

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,9 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+import { jwtConstants } from './constans';
+import { Request } from 'express';
